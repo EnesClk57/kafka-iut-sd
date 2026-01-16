f33992bea5eb9dc171fc80bba504ece5b39bb52e
Eser Enes <eeser9443@gmail.com>
2026-01-16 15:10:48 +0000

    timestamp et erp crm

---
+++ python-consumer/crm.py
@@ -0,0 +1,25 @@
+from kafka import KafkaConsumer
+import json
+
+# Define the Kafka broker and topic
+broker = 'kafka.e44394u-dev.svc.cluster.local:9092'
+topic = 'my-first-topic'
+
+# Create a Kafka consumer
+consumer = KafkaConsumer(
+    topic,
+    bootstrap_servers=[broker],
+    sasl_mechanism='SCRAM-SHA-256',
+    security_protocol='SASL_PLAINTEXT',
+    sasl_plain_username='user1',
+    sasl_plain_password='VoXRSLWisf',
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='crm'
+)
+
+print(f"Listening to topic {topic}")
+
+# Poll messages from the Kafka topic
+for message in consumer:
+    print(f"Received message: {message.value}")
--- python-producer/app.py
+++ python-producer/app.py
@@ -1,5 +1,7 @@
 from kafka import KafkaProducer
 import json
+from datetime import datetime
+import time
 
 # Define the Kafka broker and topic
 broker = 'kafka.e44394u-dev.svc.cluster.local:9092'
@@ -7,28 +9,28 @@ topic = 'my-first-topic'
 
 # Create a Kafka producer
 producer = KafkaProducer(
-   bootstrap_servers=[broker],
-  sasl_mechanism='SCRAM-SHA-256',
+    bootstrap_servers=[broker],
+    sasl_mechanism='SCRAM-SHA-256',
     security_protocol='SASL_PLAINTEXT',
     sasl_plain_username='user1',
     sasl_plain_password='VoXRSLWisf',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
-# producer = KafkaProducer(
-#     bootstrap_servers=[broker],
-#     value_serializer=lambda v: json.dumps(v).encode('utf-8')
-# )
 
-# Define the message to send
-message = {
-    'key': 'this-is-a-key',
-    'value': 'this-is-a-value'
-}
+i = 0
 
-# Send the message to the Kafka topic
-producer.send(topic, value=message)
+while True:
+    message = {
+        'key': 'consumer_instances',
+        'value': 3,
+        'timestamp': int(time.time())
+    }
+    
+    # Send the message to the Kafka topic
+    producer.send(topic, partition=0, value=message)
 
-# Ensure all messages are sent before closing the producer
-producer.flush()
+    # Ensure all messages are sent before waiting
+    producer.flush()
 
-print(f"Message sent to topic {topic}")
+    print(f"Message envoyé à {topic}")
+    time.sleep(1)
